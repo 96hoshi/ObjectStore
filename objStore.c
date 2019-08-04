80b1a3039d94e5294e20c709b8628349e8bbe0c3
96hoshi <m.locascio96@hotmail.com>
2019-08-04 20:55:09 +0200

    Added overview, defined some structs

--- objStore.c
+++ objStore.c
@@ -1,4 +1,52 @@
+// "Server", riceve richieste dai client
+
+// Ha il compito di creare per ogni client una cartella, col nome del client,
+// che conterrà gli "objects" dell'utente.
+// Gli objects sono, in questo caso, array di caratteri che verranno
+// memorizzati in appositi file (con lo stesso nome dell'object).
+// Tutte le cartelle degli utenti sono localizzate nella cartella data,
+// creata dal server.
+// Tutti gli object e i client hanno nomi che rispettano lo standard POSIX
+// e sono univoci all'interno dello stesso spazio di memorizzazione
+
+// L'objStore attende il collegamento su objstore.sock.
+// Crea  un thread per le richieste di ogni client.
+// Quando riceve un messsaggio del client viene tradotto grazie alla lib message.h
+// Che verrà utilizzata anche per inviare la risposta.
+// Risponde sempre!!
+// Quando riceve un segnale risponde il prima possibile mantenendo
+// l'objstore in uno stato consistente.
+
+// Quando il server riceve il segnare SIGUSR1 stampa su stout info
+// sullo stato del server, quali:
+// 	-#client connessi
+// 	-#oggetti nello store
+// 	-size totale dello store
+// 	-...
+// La libreria stats.h si occuperà di stampare tali statistiche.
+// A differenza delle stats del client, per le stats del server servirà
+// una struttura condivisa tra i thread e gestita in maniera threadsafe
+// con apposite lock
+
+// Strutture dati:
+// -users: lista di
+// -	char* name
+// -	char* path dir ?
+// -	objects* object
+// -	users* next
+// -objects: lista di
+// 		-char* name
+// 		-int len
+// 		-objects* next
+
+// Necessito di una libreria per la gestione di tali strutture dati
+// Nome da definire
+
+// Necessito di una libreria per la gestione dei file
+// Nome da definire
+
 #include "message.h"
+#include "stats.h"
 
 int main(int argc, char *argv[]) {
 
