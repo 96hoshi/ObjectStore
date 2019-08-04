51f44ab4340187aab299d429c2055a861bdcf72c
96hoshi <m.locascio96@hotmail.com>
2019-08-04 20:51:28 +0200

    Added overview

--- client.c
+++ client.c
@@ -1,12 +1,30 @@
+// Client che comunica con il server attraverso la libreria protocol.h
+
+// Per comunicare manda una richiesta di registrazione.
+// Successivamente può mandare tre tipi di richieste al server:
+// 	1.creare e memorizzare oggetti 
+// 		(20  oggetti di dimensione 100 <= dim <= 100.000 bytes
+// 		con nomi convenzionali contenenti dati facilmente verificabili)
+//				TODO:scegliere il contenuto dei files
+// 	2.recupero di oggetti e verifica della correttezza
+// 	3.cancellare oggetti
+
+// Il client riceve da linea di comando
+// 	-nome del cliente
+// 	-un numero 1-3 per sapere quale "test" eseguire
+// Finito il test stampa su stout un rapporto contenente
+// 	-#operazioni effettuate
+// 	-#operazioni concluse con successo
+// 	-#operazioni fallite
+// 	-...
+// La stampa del rapporto sarà affidata a stats.h
+// Terminati i test il client deve disconnetersi.
+
 #include "protocol.h"
-#include "message.h"
-//#include "stats.h"
+#include "stats.h"
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-
-	printf("Sono passato da protocol : %d\n", protocolfun());
-	printf("E anche da message : %d\n", messagefun());
 
 	return 0;
 }
