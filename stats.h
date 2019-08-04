2b78b7f236cb37601f16f1a08ca7f06872d26fa1
96hoshi <m.locascio96@hotmail.com>
2019-08-04 20:58:39 +0200

    Added short overview

--- stats.h
+++ stats.h
@@ -1,3 +1,17 @@
+//Responsabile della stampa delle statistiche di client e server
+
+//Client:
+// 	-#operazioni effettuate
+// 	-#operazioni concluse con successo
+// 	-#operazioni fallite
+// 	-...
+
+//Server:
+// 	-#client connessi
+// 	-#oggetti nello store
+// 	-size totale dello store
+// 	-...
+
 #ifndef STATS_H
 #define STATS_H
 
