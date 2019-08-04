a397483bf0ede841bf8758f2cfb4fd8fc511960f
96hoshi <m.locascio96@hotmail.com>
2019-08-04 20:52:55 +0200

    Added overview and defined struct

--- message.c
+++ message.c
@@ -1,7 +1,20 @@
-#include "message.h"
-#include "ops.h"
+// Traduce i messaggi tra client (request) e server (response) in strutture "leggibili".
 
-int messagefun()
-{
-	return 20;
-}
+// Messaggio = "header" ,codificato in ASCII, è una stringa di testo terminata da "\n"
+// 			a cui può succedere un blocco di dati
+// Cosa posso trovare nei messaggi:
+// 	OP 		= array di caratteri che indica l'operazione da eseguire
+// 	name 	= array di caratteri che contiene il nome del client o del dato
+// 	len 	= lunghezza in byte del dato, codificato in ASCII
+// 	data 	= blocco binario i lunghezza len, posto dopo lo "\n"
+// 	message = array di caratteri che contiene in messaggio di errore del server
+
+// Struttura dati:
+// -msg:
+// 	-int OP
+// 	-char* name
+// 	-int len
+// 	-void* data (che conterrà anche message in caso)
+
+#include "message.h"
+//#include "ops.h"
--- message.h
+++ message.h
@@ -1,6 +1,6 @@
 #ifndef MESSAGE_H
 #define MESSAGE_H
 
-int messagefun();
+
 
 #endif
