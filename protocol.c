303ecbdd534c022a59f8761e8f9e89f522944824
96hoshi <m.locascio96@hotmail.com>
2019-08-21 11:13:25 +0200

    Updated comments

--- protocol.c
+++ protocol.c
@@ -21,43 +21,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// inizia la connessione all'object store, registrando il cliente con il
-// name dato. Restituisce true se la connessione ha avuto successo,
-// false altrimenti. Notate che la connessione all'object 
-// store è globale per il client.
 int os_connect(char *name)
 {
+
 	return 0;
 }
 
-// richiede all'object store la memorizzazione dell'oggetto puntato da block,
-// per una lunghezza len, con il nome name. Restituisce true se la
-// memorizzazione ha avuto successo, false altrimenti.
 int os_store(char *name, void *block, size_t len)
 {
 	return 0;
 }
 
-// recupera dall'object store l'oggetto precedentemente
-// memorizzatato sotto il nome name. Se il recupero ha avuto successo,
-// restituisce un puntatore a un blocco di memoria,
-// allocato dalla funzione, contenente i dati precedentemente memorizzati.
-// In caso di errore, restituisce NULL.
 void *os_retrieve(char *name)
 {
  return NULL;
 }
 
-// cancella l'oggetto di nome name precedentemente memorizzato.
-// Restituisce true se la cancellazione ha avuto successo, false altrimenti.
 int os_delete(char *name)
 {
 	return 0;
 }
 
-// chiude la connessione all'object store.
-// Restituisce true se la disconnessione ha avuto successo,
-// false in caso contrario.
 int os_disconnect()
 {
 	return 0;
--- protocol.h
+++ protocol.h
@@ -7,11 +7,25 @@
 // name dato. Restituisce true se la connessione ha avuto successo,
 // false altrimenti. Notate che la connessione all'object 
 // store è globale per il client.
+
+// creazione stringa del messaggio
+// creazione fd_socket client "globale"
+// connessione su socket locale col server
+// scrittura del messaggio nel canale
+// libero memoria per il messaggio ?????
+// creo un buffer per la lettura
+// lettura del messaggio di risposta
+// traduzione messaggio
+// libero buffer
+// ritorno messaggio tradotto (true o false)
 int os_connect(char *name);
 
 // richiede all'object store la memorizzazione dell'oggetto puntato da block,
 // per una lunghezza len, con il nome name. Restituisce true se la
 // memorizzazione ha avuto successo, false altrimenti.
+
+// creazione del messaggio con i parametri dati
+// connessione server
 int os_store(char *name, void *block, size_t len);
 
 // recupera dall'object store l'oggetto precedentemente
@@ -19,15 +33,25 @@ int os_store(char *name, void *block, size_t len);
 // restituisce un puntatore a un blocco di memoria,
 // allocato dalla funzione, contenente i dati precedentemente memorizzati.
 // In caso di errore, restituisce NULL.
+
+// creazione del messaggio con il nome
+// connessione server
 void *os_retrieve(char *name);
 
 // cancella l'oggetto di nome name precedentemente memorizzato.
 // Restituisce true se la cancellazione ha avuto successo, false altrimenti.
+
+// creazione del messaggio con il nome
+// connessione server
 int os_delete(char *name);
 
 // chiude la connessione all'object store.
 // Restituisce true se la disconnessione ha avuto successo,
 // false in caso contrario.
+
+// creazione del messaggio
+// connessione server
+// chiude la socket
 int os_disconnect();
 
 #endif
