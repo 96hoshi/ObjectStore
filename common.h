247846031aa76e4818714c1eaf834a29199b4090
96hoshi <m.locascio96@hotmail.com>
2019-08-22 15:39:12 +0200

    Added buffers and socket defines

--- common.h
+++ common.h
@@ -1,7 +1,38 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #define TRUE 1
 #define FALSE 0
+
+#define MAX_BUFF 1024
+
+#define UNIX_PATH_MAX 108
+#define SOCKNAME "./objstore.sock"
+
+inline static void check_calloc(void *buff, void (*err_callback)(void))
+{
+	if (buff == NULL) {
+		if (err_callback == NULL) {
+			perror("ERR: calloc error\n");
+		} else {
+			err_callback();
+		}
+		exit(EXIT_FAILURE);
+	}
+}
+
+inline static void invalid_operation(void (*err_callback)(void))
+{
+	if (err_callback == NULL) {
+		perror("ERR: invalid operation\n");
+	} else {
+		err_callback();
+	}
+	exit(EXIT_FAILURE);
+}
+
 
 #endif
