15cf6052ae66a47ccf10d8ae811e764b4b2f4691
96hoshi <m.locascio96@hotmail.com>
2019-08-30 14:29:01 +0200

    Added signal to send to server

--- test_clients.sh
+++ test_clients.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+
 ./objStore &
+serverpid=$!
+sleep 1
 
 for i in `seq 1 50`; do
 	./client ${i} 1 &
 	pids[${i}]=$!
 done
 
+
 # wait for all pids
 for pid in ${pids[*]}; do
 	wait $pid
 done
+
+kill -SIGUSR1 $serverpid
